9658226d3b32c50e4c9113a77204b3b36415347f
Vishal Dixit <93127811+0628Vishal@users.noreply.github.com>
2023-02-01 13:16:16 +0530

    Update streamlit_app.py
    added a bunch of text pertaining to the menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,9 @@
 import streamlit
 
 streamlit.title('My parents new Healthy Diner!')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 and blueberry omelettes')
+streamlit.text('Kale, Spinach and Rcoket Smoothie')
+streamlit.text('hard boiled free-range egg')
+
 
