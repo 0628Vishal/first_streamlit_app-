6a766e4974530adeaec6ff04345f18dfae88493c
Vishal Dixit <93127811+0628Vishal@users.noreply.github.com>
2023-02-08 00:01:37 +0530

    Update streamlit_app.py
    changing visual appeal by re-ordering panels

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,15 +2,6 @@ import snowflake.connector
 import streamlit
 import pandas 
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
-streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
-
-
-
 streamlit.title('My parents new Healthy Diner!')
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 and blueberry omelettes')
@@ -32,10 +23,20 @@ streamlit.write('The user entered ', fruit_choice)
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 
-fruit_choice_one = streamlit.text_input('What fruit would you like to add?','Jackfruit')
-streamlit.write('Thanks for adding', fruit_choice_one)
-
 # write your own comment -what does the next line do? 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("select * from fruit_load_list")
+my_data_rows = my_cur.fetchall()
+streamlit.header("The fruit load list contains:")
+streamlit.dataframe(my_data_rows)
+
+
+fruit_choice_one = streamlit.text_input('What fruit would you like to add?','Jackfruit')
+streamlit.write('Thanks for adding', fruit_choice_one)
+
+
